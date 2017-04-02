763dd7a3a45c228e308bb52f7a24d4263c4a883e
Adam Ringhede <adamringhede@live.com>
2017-04-02 22:08:31 +0200

    cluster: Change PartitionCollection to use an AVL-tree instead of a Red-Black tree

--- cluster/partition.go
+++ cluster/partition.go
@@ -1,7 +1,7 @@
 package cluster
 
 import (
-	"github.com/emirpasic/gods/trees/redblacktree"
+	"github.com/emirpasic/gods/trees/avltree"
 )
 
 type Partition struct {
@@ -10,13 +10,13 @@ type Partition struct {
 }
 
 // PartitionCollection is for partition look ups
-// backed by a red black tree.
+// backed by an AVL-tree
 type PartitionCollection struct {
-	tree	*redblacktree.Tree
+	tree 	*avltree.Tree
 }
 
 func NewPartitionCollection() *PartitionCollection {
-	return &PartitionCollection{redblacktree.NewWithIntComparator()}
+	return &PartitionCollection{avltree.NewWithIntComparator()}
 }
 
 func (c *PartitionCollection) Put(partition *Partition) {
@@ -28,71 +28,43 @@ func (c *PartitionCollection) Remove(key int) {
 }
 
 func (c *PartitionCollection) Get(key int) *Partition {
-	iter, ok := c.findNode(key)
-	if ok {
-		return iter.Value().(*Partition)
+	node, ok := c.findNode(key)
+	if !ok {
+		return nil
 	}
-	return nil
+	return node.Value.(*Partition)
 }
 
 func (c *PartitionCollection) GetMultiple(key int, count int) []*Partition {
 	res := []*Partition{}
-	found := make(map[int]bool)
-	iter, ok := c.findNode(key)
+	node, ok := c.findNode(key)
 	if !ok {
 		return res
 	}
+	found := make(map[int]bool)
 	for len(res) <= count {
-		k := iter.Key().(int)
-		if _, ok := found[k]; ok {
-			res = append(res, iter.Value().(*Partition))
+		k := node.Key.(int)
+		if _, ok := found[k]; !ok {
+			res = append(res, node.Value.(*Partition))
 			found[k] = true
 		} else {
 			break
 		}
-		if !iter.Next() {
-			iter.First()
+		node = node.Next()
+		if node == nil {
+			node = c.tree.Left()
 		}
 	}
 	return res
 }
 
-func (c *PartitionCollection) findNode(key int) (*redblacktree.Iterator, bool) {
+func (c *PartitionCollection) findNode(key int) (*avltree.Node, bool) {
 	if c.tree.Size() == 0 {
 		return nil, false
 	}
-	current := c.tree.Root
-	for {
-		switch c.tree.Comparator(key, current.Key) {
-		case 1:
-			if current.Right == nil {
-				return &redblacktree.Iterator{
-					c.tree,
-					current,
-					1,
-				}, true
-			}
-			current = current.Right
-		case -1:
-			if current.Left == nil {
-				iter := &redblacktree.Iterator{
-					c.tree,
-					current,
-					1,
-				}
-				if !iter.Prev() {
-					iter.Last()
-				}
-				return iter, true
-
-			}
-			current = current.Left
-		default:
-			return &redblacktree.Iterator{
-				c.tree,
-				current,
-				1,
-			}, true
-		}
+	node, ok := c.tree.Floor(key)
+	if !ok {
+		node = c.tree.Left()
 	}
+	return node, true
 }
--- cluster/partition_test.go
+++ cluster/partition_test.go
@@ -7,11 +7,34 @@ import (
 
 func TestPartitionCollection_Get(t *testing.T) {
 	collection := NewPartitionCollection()
-	collection.Put(newPartition(1))
+	collection.Put(newPartition(2))
 
-	assert.Equal(t, 1, collection.Get(2).Token)
+	assert.Equal(t, 2, collection.Get(3).Token)
+	assert.Equal(t, 2, collection.Get(1).Token)
+}
+
+func TestPartitionCollection_GetMultiple(t *testing.T) {
+	collection := NewPartitionCollection()
+	collection.Put(newPartition(2))
+	collection.Put(newPartition(5))
+	collection.Put(newPartition(9))
+
+	res := collection.GetMultiple(3, 3)
+	assert.Equal(t, 2, res[0].Token)
+	assert.Equal(t, 5, res[1].Token)
+	assert.Equal(t, 9, res[2].Token)
+
+	res2 := collection.GetMultiple(3, 4)
+	assert.Equal(t, 3, len(res2))
+	assert.Equal(t, 2, res2[0].Token)
+	assert.Equal(t, 5, res2[1].Token)
+	assert.Equal(t, 9, res2[2].Token)
+
+	res3 := collection.GetMultiple(10, 2)
+	assert.Equal(t, 9, res3[0].Token)
+	assert.Equal(t, 2, res3[1].Token)
 }
 
 func newPartition(token int) *Partition {
-	return &Partition{1, &Node{}}
+	return &Partition{token, &Node{}}
 }
