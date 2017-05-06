630274924ddc7e291d6769d83ce43ffd4876ca47
Adam Ringhede <adamringhede@Adams-MacBook-Pro.local>
2017-05-06 14:06:56 +0200

    cluster: Fix base dir for etcd token storage

--- cluster/token_storage.go
+++ cluster/token_storage.go
@@ -16,7 +16,7 @@ type tokenStorage interface {
 	Release()
 }
 
-const tokenStorageBaseDir = "influxdbCluster"
+const etcdStorageBaseDir = "influxdbCluster"
 
 type etcdTokenStorage struct {
 	client client.Client
@@ -26,13 +26,13 @@ type etcdTokenStorage struct {
 // Assign sets a token to refer to a certain node
 func (s *etcdTokenStorage) Assign(token int, node string) error {
 	val := node
-	_, err := s.kapi.Set(context.Background(), createEtcdTokenPath(token), val, nil)
+	_, err := s.kapi.Set(context.Background(), createEtcdTokenPath(token, "reservedTokens"), val, nil)
 	return err
 }
 
 // Get return a map of all tokens and the nodes they refer to.
 func (s *etcdTokenStorage) Get() (map[int]string, error) {
-	resp, getErr := s.kapi.Get(context.Background(), tokenStorageBaseDir + "/reservedTokens/", nil)
+	resp, getErr := s.kapi.Get(context.Background(), etcdStorageBaseDir+ "/tokens/", nil)
 	if getErr != nil {
 		return nil, getErr
 	}
@@ -50,10 +50,17 @@ func (s *etcdTokenStorage) Get() (map[int]string, error) {
 // Reserve should prevent other nodes from assigning a token to itself as some other is
 // currently importing data for it to later assign the token to itself.
 func (s *etcdTokenStorage) Reserve(token int, node string) (bool, error) {
-	resp, getErr := s.kapi.Get(context.Background(), createEtcdTokenPath(token), nil)
+	resp, getErr := s.kapi.Get(context.Background(), createEtcdTokenPath(token, "reservedTokens"), nil)
 	// Do not reserve if it is reserved by some other node
-	if getErr != nil && getErr.(client.Error).Code != client.ErrorCodeKeyNotFound  {
-		return false, getErr
+	if getErr != nil {
+		switch e := getErr.(type) {
+		case client.Error:
+			if e.Code != client.ErrorCodeKeyNotFound {
+				return false, getErr
+			}
+		default:
+			return false, getErr
+		}
 	}
 	if resp != nil && resp.Node.Value != node {
 		return false, nil
@@ -61,13 +68,13 @@ func (s *etcdTokenStorage) Reserve(token int, node string) (bool, error) {
 	val := node
 	opts := client.SetOptions{}
 	opts.TTL = time.Minute
-	_, err := s.kapi.Set(context.Background(), createEtcdTokenPath(token), val, nil)
+	_, err := s.kapi.Set(context.Background(), createEtcdTokenPath(token, "reservedTokens"), val, nil)
 	return err == nil, err
 }
 
 // Release is called after a node has finished importing data for a token range
 func (s *etcdTokenStorage) Release(token int) error {
-	_, err := s.kapi.Delete(context.Background(), createEtcdTokenPath(token), nil)
+	_, err := s.kapi.Delete(context.Background(), createEtcdTokenPath(token, "reservedTokens"), nil)
 	return err
 }
 
@@ -91,6 +98,6 @@ func (s *etcdTokenStorage) Open(entrypoints []string) {
 	s.kapi = client.NewKeysAPI(c)
 }
 
-func createEtcdTokenPath(token int) string {
-	return tokenStorageBaseDir + "/reservedTokens/" + strconv.Itoa(token)
+func createEtcdTokenPath(token int, path string) string {
+	return etcdStorageBaseDir + "/" + path + "/" + strconv.Itoa(token)
 }
