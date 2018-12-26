1295e780deef784ea09e98b53884afb02d3e19f9
Adam Ringhede <adamringhede@live.com>
2018-12-26 17:38:51 +0100

    Remove unused code

--- syncing/models.go
+++ syncing/models.go
@@ -1,25 +1,5 @@
 package syncing
 
-import "github.com/influxdata/influxdb/models"
-
-// Message represents a user-facing message to be included with the result.
-type message struct {
-	Level string `json:"level"`
-	Text  string `json:"text"`
-}
-
-type result struct {
-	StatementID int           `json:"statement_id"`
-	Series      []*models.Row `json:"series,omitempty"`
-	Messages    []*message    `json:"messages,omitempty"`
-	Partial     bool          `json:"partial,omitempty"`
-	Err         string        `json:"error,omitempty"`
-}
-
-type response struct {
-	Results []result `json:"results"`
-}
-
 type RetentionPolicy struct {
 	Name               string
 	Duration           string
